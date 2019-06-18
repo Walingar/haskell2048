2ad89c47a330e577de593adc8c418f37d02d215b
Nikolay.Rykunov <Nikolay.Rykunov@jetbrains.com>
2019-06-18 19:27:40 +0300

    Implement loading saved data

--- src/Game.hs
+++ src/Game.hs
@@ -4,6 +4,7 @@ module Game
   ( startGame
   ) where
 
+import Control.Applicative ((<|>))
 import Control.Monad (when)
 import Control.Monad.Reader (ReaderT, ask, lift, runReaderT)
 import Data.Foldable (forM_)
@@ -11,8 +12,12 @@ import Data.IORef (IORef, modifyIORef, newIORef, readIORef, writeIORef)
 import qualified Data.Vector as V
 import qualified Data.Vector.Unboxed as VU
 import qualified Data.Vector.Unboxed.Mutable as MU
+import Data.Void (Void)
 import System.IO (BufferMode (..), hSetBuffering, stdin)
 import System.Random (newStdGen, randomRs)
+import Text.Megaparsec (Parsec, eof, parse)
+import Text.Megaparsec.Char (char, digitChar, eol, space, string)
+import Text.Megaparsec.Error (errorBundlePretty)
 
 data Move
   = TopMove
@@ -195,11 +200,11 @@ move = do
     's' -> moveImpl DownMove
     'a' -> moveImpl LeftMove
     'c' -> do
-      lift $ putStrLn "Game data has been saved!"
       saveData
+      lift $ putStrLn "Game data has been saved!"
     'l' -> do
+      loadData
       lift $ putStrLn "Your data has been loaded!"
-      undefined
     _ -> lift $ putStrLn ("Unexpected action: " ++ show userAction)
 
 turn :: Bool -> ReaderT GameData IO ()
@@ -272,14 +277,75 @@ saveData = do
     saveData' :: GameData -> IO String
     saveData' GameData {score = curScoreRef, field = Field curField} = do
       curScore <- readIORef curScoreRef
-      res <- newIORef ""
-      modifyIORef res (\s -> s ++ "Score: " ++ show curScore ++ "\n")
+      res <- newIORef ("Score: " ++ show curScore ++ "\n")
       forM_ [0 .. maxSize - 1] $ \i -> do
         let row = curField V.! i
         forM_ [0 .. maxSize - 1] $ \j -> do
           el <- MU.read row j
           modifyIORef res (\s -> concat [s, "Cell ", show i, " ", show j, ": ", show el, "\n"])
       readIORef res
+
+type Parser = Parsec Void String
+
+parseIntString :: Parser String
+parseIntString = do
+  ch <- fmap Left digitChar <|> fmap Right (pure ())
+  case ch of
+    Left dgt -> do
+      suffix <- parseIntString
+      return $ dgt : suffix
+    Right _ -> return []
+
+parseScore :: Parser Int
+parseScore = do
+  i <- string "Score:" *> space *> parseIntString
+  _ <- eol
+  return $ read i
+
+parseCell :: Parser (Int, Int, Int)
+parseCell = do
+  i <- string "Cell" *> space *> parseIntString
+  j <- space *> parseIntString
+  el <- char ':' *> space *> parseIntString
+  _ <- eol
+  return (read i, read j, read el)
+
+parseCells :: Parser [(Int, Int, Int)]
+parseCells = do
+  line <- fmap Left parseCell <|> fmap Right eof
+  case line of
+    Left cell -> do
+      parsedTail <- parseCells
+      return $ cell : parsedTail
+    Right _ -> return []
+
+data ParsedData = ParsedData
+  { parsedField :: [(Int, Int, Int)]
+  , parsedScore :: Int
+  }
+
+parseData :: Parser ParsedData
+parseData = do
+  curScore <- parseScore
+  curField <- parseCells
+  return $ ParsedData {parsedField = curField, parsedScore = curScore}
+
+copyLoadedField :: [(Int, Int, Int)] -> Field -> IO ()
+copyLoadedField [] _ = return ()
+copyLoadedField ((i, j, el):xs) curField@(Field vector) = do
+  let row = vector V.! i
+  MU.write row j el
+  copyLoadedField xs curField
+
+loadData :: ReaderT GameData IO ()
+loadData = do
+  input <- lift $ readFile "save"
+  case parse parseData "parse_log" input of
+    Left err -> lift $ putStrLn $ errorBundlePretty err
+    Right ParsedData {parsedField = curParsedField, parsedScore = curScore} -> do
+      GameData {score = curScoreRef, field = curField} <- ask
+      lift $ writeIORef curScoreRef curScore
+      lift $ copyLoadedField curParsedField curField
 
 startGame :: IO ()
 startGame = do
